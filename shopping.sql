5c35b5c2918c337a52fa38d5f417c0f1eb57f9ec
SL-CodeWorrier <chathuathnayaka@gmail.com>
2024-03-26 10:04:55 +0530

    initial commit

---
+++ shopping.sql
@@ -0,0 +1,290 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Mar 26, 2024 at 04:38 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `shopping`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `brand_detail`
+--
+
+CREATE TABLE `brand_detail` (
+  `brand_id` int(11) NOT NULL,
+  `brand_name` varchar(150) NOT NULL DEFAULT '',
+  `status` int(1) NOT NULL DEFAULT 1 COMMENT '1: active, 2: deleted',
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart_detail`
+--
+
+CREATE TABLE `cart_detail` (
+  `cart_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `prod_id` int(11) NOT NULL DEFAULT 0,
+  `qty` int(11) NOT NULL DEFAULT 1,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `category_detail`
+--
+
+CREATE TABLE `category_detail` (
+  `cat_id` int(11) NOT NULL,
+  `cat_name` varchar(100) NOT NULL DEFAULT '',
+  `image` varchar(75) NOT NULL DEFAULT '',
+  `color` varchar(10) NOT NULL DEFAULT '000000',
+  `status` int(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 2 = delete',
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favorite_detail`
+--
+
+CREATE TABLE `favorite_detail` (
+  `fav_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL DEFAULT 0,
+  `user_id` int(11) NOT NULL DEFAULT 0,
+  `status` int(1) NOT NULL DEFAULT 1,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_detail`
+--
+
+CREATE TABLE `image_detail` (
+  `img_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL DEFAULT 0,
+  `image` varchar(75) NOT NULL DEFAULT '',
+  `status` int(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 2 = delete',
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `offer_detail`
+--
+
+CREATE TABLE `offer_detail` (
+  `offer_id` int(11) NOT NULL,
+  `prod_id` int(11) NOT NULL DEFAULT 0,
+  `price` double NOT NULL DEFAULT 0,
+  `start_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `end_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `status` int(11) NOT NULL DEFAULT 1,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_detail`
+--
+
+CREATE TABLE `product_detail` (
+  `prod_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL DEFAULT 0,
+  `brand_id` int(11) NOT NULL DEFAULT 0,
+  `type_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL DEFAULT '',
+  `detail` varchar(5000) NOT NULL DEFAULT '',
+  `unit_value` varchar(20) NOT NULL DEFAULT '1',
+  `price` double NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 1 COMMENT '1: active, 2: delete',
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `type_detail`
+--
+
+CREATE TABLE `type_detail` (
+  `type_id` int(11) NOT NULL,
+  `type_name` varchar(100) NOT NULL DEFAULT '',
+  `image` varchar(75) NOT NULL DEFAULT '',
+  `color` varchar(8) NOT NULL DEFAULT '000000',
+  `status` int(1) NOT NULL DEFAULT 1 COMMENT '1 = active, 2 = delete',
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_detail`
+--
+
+CREATE TABLE `user_detail` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(75) NOT NULL DEFAULT '',
+  `name` varchar(100) NOT NULL DEFAULT '',
+  `email` varchar(100) NOT NULL DEFAULT '',
+  `mobile` varchar(15) NOT NULL DEFAULT '',
+  `password` varchar(100) NOT NULL DEFAULT '',
+  `status` int(1) NOT NULL DEFAULT 1,
+  `created_date` datetime NOT NULL DEFAULT current_timestamp(),
+  `modify_date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `brand_detail`
+--
+ALTER TABLE `brand_detail`
+  ADD PRIMARY KEY (`brand_id`);
+
+--
+-- Indexes for table `cart_detail`
+--
+ALTER TABLE `cart_detail`
+  ADD PRIMARY KEY (`cart_id`);
+
+--
+-- Indexes for table `category_detail`
+--
+ALTER TABLE `category_detail`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indexes for table `favorite_detail`
+--
+ALTER TABLE `favorite_detail`
+  ADD PRIMARY KEY (`fav_id`);
+
+--
+-- Indexes for table `image_detail`
+--
+ALTER TABLE `image_detail`
+  ADD PRIMARY KEY (`img_id`);
+
+--
+-- Indexes for table `offer_detail`
+--
+ALTER TABLE `offer_detail`
+  ADD PRIMARY KEY (`offer_id`);
+
+--
+-- Indexes for table `product_detail`
+--
+ALTER TABLE `product_detail`
+  ADD PRIMARY KEY (`prod_id`);
+
+--
+-- Indexes for table `type_detail`
+--
+ALTER TABLE `type_detail`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `brand_detail`
+--
+ALTER TABLE `brand_detail`
+  MODIFY `brand_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `cart_detail`
+--
+ALTER TABLE `cart_detail`
+  MODIFY `cart_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `category_detail`
+--
+ALTER TABLE `category_detail`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `favorite_detail`
+--
+ALTER TABLE `favorite_detail`
+  MODIFY `fav_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `image_detail`
+--
+ALTER TABLE `image_detail`
+  MODIFY `img_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `offer_detail`
+--
+ALTER TABLE `offer_detail`
+  MODIFY `offer_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `product_detail`
+--
+ALTER TABLE `product_detail`
+  MODIFY `prod_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `type_detail`
+--
+ALTER TABLE `type_detail`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user_detail`
+--
+ALTER TABLE `user_detail`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
